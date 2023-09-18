5be1b487af745d71fbc875fc245c40983bf1e0e7
khaledRokaya <112181079+khaledrokaya@users.noreply.github.com>
2023-09-18 21:06:14 +0300

    Time: 0 ms (100%), Space: 6.4 MB (5.31%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int l = 1, r = 65535, mid;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            int tmp = ((long long)mid * (mid + 1)) / 2;
+            if (tmp > n) {
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+};
