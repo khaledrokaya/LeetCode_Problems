b3e1e9381ac0d3eb595016bf48dc641fe092e95f
khaledRokaya <112181079+khaledrokaya@users.noreply.github.com>
2023-09-21 02:41:25 +0300

    Time: 231 ms (40.85%), Space: 135 MB (44.62%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,29 +1,43 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq;
-        vector<int> ans; 
-        int n = nums.size(); 
+        deque<int> dq; // Deque to store indices of elements in the window
+        vector<int> ans; // Vector to store the result
+        int n = nums.size(); // Total number of elements in the input vector
+
+        // Check for invalid input
         if (n == 0 || k <= 0 || k > n) {
-            return ans; 
+            return ans; // Return an empty vector for invalid inputs
         }
+
+        // Process the first sliding window of size k
         for (int i = 0; i < k; i++) {
+            // Remove indices of elements that are less than the current element
+            // from the back of the deque as they won't be the maximum elements
             while (!dq.empty() && nums[i] >= nums[dq.back()]) {
                 dq.pop_back();
             }
-            dq.push_back(i); 
+            dq.push_back(i); // Add the current element's index to the back of the deque
         }
-        ans.push_back(nums[dq.front()]); 
+
+        ans.push_back(nums[dq.front()]); // The front of the deque contains the index of the maximum element
+
+        // Slide the window and find maximums for the rest of the elements
         for (int i = k; i < nums.size(); i++) {
             if (i - dq.front() >= k) {
-                dq.pop_front(); 
+                dq.pop_front(); // If the front element of the deque is outside the current window, remove it
             }
+
+            // Remove indices of elements that are less than the current element
+            // from the back of the deque as they won't be the maximum elements
             while (!dq.empty() && nums[i] >= nums[dq.back()]) {
                 dq.pop_back();
             }
-            dq.push_back(i); 
-            ans.push_back(nums[dq.front()]); 
+
+            dq.push_back(i); // Add the current element's index to the back of the deque
+            ans.push_back(nums[dq.front()]); // The front of the deque contains the index of the maximum element
         }
-        return ans; 
+
+        return ans; // Return the vector containing maximums for each sliding window
     }
 };
